6def60f98611c6ff144d32bc2f3622d608017e5e
Charles <charlo22@hotmail.fr>
2020-09-12 17:27:09 +0200

    Log to file bc pm2 nul

--- bot.py
+++ bot.py
@@ -196,6 +196,11 @@ class MyClient(discord.Client):
             else:
                 await message.channel.send("T'as pas le droit cheh")
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 client = MyClient()
 client.run(config.token)
