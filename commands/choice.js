101d75c0ae3182035b45463f122ad70ebb6a7626
Charles Attend <charles.vin@outlook.fr>
2021-10-30 21:26:10 +0200

    Feature: option to hide choice reply

--- commands/choice.js
+++ commands/choice.js
@@ -2,14 +2,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('choices')
+		.setName('choose')
 		.setDescription('Pick a random choice from list')
 		.addStringOption(option =>
-			option.setName('input')
+			option.setName('choices')
 				.setDescription('Enter each choice separated by a space')
-				.setRequired(true)),
+				.setRequired(true))
+		.addBooleanOption(option =>
+			option.setName('hide')
+				.setDescription('Reply in an ephemeral message')),
+
 	async execute(interaction) {
 		const choiceList = interaction.options.getString('choices').split(' ');
-		await interaction.reply(choiceList[Math.floor(Math.random() * choiceList.length)]);
+		await interaction.reply({ content: choiceList[Math.floor(Math.random() * choiceList.length)], ephemeral: interaction.options.getBoolean('hide') });
 	},
 };
