cbedab5545480775bfc7d5ff9433dc5db036245a
Charles Attend <charles.vin@outlook.fr>
2021-10-30 20:15:23 +0200

    small change on code organisation

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,12 @@
+const wtfImageList = ['./img/wtf.gif', './img/wtf2.gif'];
+
 module.exports = {
 	name: 'messageCreate',
-	wtfImageList: ['./img/wtf.gif', './img/wtf2.gif'],
 	async execute(message) {
 		console.log('new message :', message.content);
 		const content = message.content.toLowerCase();
 		if (content.startsWith('wtf')) {
-			await message.channel.send({ files: [this.wtfImageList[Math.floor(Math.random() * this.wtfImageList.length)]] });
+			await message.channel.send({ files: [wtfImageList[Math.floor(Math.random() * wtfImageList.length)]] });
 		}
 	},
 };
