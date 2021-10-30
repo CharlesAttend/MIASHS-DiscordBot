b8865817da9702aea75c63a3fdbb5e00e931e18a
Charles Attend <charles.vin@outlook.fr>
2021-10-30 19:26:53 +0200

    catching await error

--- commands/roles.js
+++ commands/roles.js
@@ -64,30 +64,35 @@ const setRole = async (interaction, years, option) => {
 		// 'L2': await interaction.guild.roles.fetch('738818587292074104'),
 		// 'L3': await interaction.guild.roles.fetch('738833214021566556'),
 		'L1': await interaction.guild.roles.fetch('459809474371125250'),
-		'eco':await interaction.guild.roles.fetch('738854612294959144'),
+		'Eco': await interaction.guild.roles.fetch('738854612294959144'),
 	};
 	await interaction.member.roles.remove(Object.values(roles));
 	await interaction.member.roles.add([roles[years], roles[option]]);
 };
 
 module.exports = {
-	data : new SlashCommandBuilder()
+	data: new SlashCommandBuilder()
 		.setName('roles')
 		.setDescription('Choisir ses rôles !'),
 	async execute(interaction) {
-		const filter = i => {
-			i.deferUpdate();
-			return i.user.id === interaction.user.id;
-		};
-		// asking current years and waiting for reply 
-		const message = await interaction.reply({ content: 'Choisie ton années ! 🔢', ephemeral: true, components: [YearsRow], fetchReply: true });
-		const years = await message.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 });
-		// asking option and waiting for reply
-		await interaction.editReply({ content: 'Maintenant choisie ton parcourt ! 🔀', ephemeral: false, components: [OptionRow] });
-		const option = await message.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 });
-		// setting roles
-		await setRole(interaction, years.values, option.values);
-		// validating
-		await interaction.editReply({ content: 'Tes rôles ont été actualisé ✅', ephemeral: false, components: [OptionRow] });
+		try {
+			const filter = i => {
+				i.deferUpdate();
+				return i.user.id === interaction.user.id;
+			};
+			// asking current years and waiting for reply 
+			const message = await interaction.reply({ content: 'Choisie ton années ! 🔢', ephemeral: false, components: [YearsRow], fetchReply: true });
+			const years = await message.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 });
+			// asking option and waiting for reply
+			await interaction.editReply({ content: 'Maintenant choisie ton parcourt ! 🔀', ephemeral: true, components: [OptionRow] });
+			const option = await message.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 });
+			// setting roles
+			await setRole(interaction, years.values, option.values);
+			// validating
+			await interaction.editReply({ content: 'Tes rôles ont été actualisé ✅', ephemeral: true });
+		}
+		catch (error) {
+			console.log(error);
+		}
 	},
 };
