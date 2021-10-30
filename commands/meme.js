ab26e273beb9b4721de5df86f259b5d290c2b072
Charles Attend <charles.vin@outlook.fr>
2021-10-30 20:59:38 +0200

    Make all meme in one choice command

--- commands/meme.js
+++ commands/meme.js
@@ -1,36 +1,33 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const EhehImageList = ['img/eh eh boi1.webp', 'img/eh eh boi2.jpg', 'img/eh eh boi3.gif', 'img/eh eh boi4.png'];
 const ExcusemeImageList = ['img/excuseme.jpg', 'img/excuseme3.jpg', 'img/excuseme2.jpg'];
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('meme')
 		.setDescription('Send the choose meme')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('fbi')
-				.setDescription('Send `FBI` meme'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('police')
-				.setDescription('Send `police` meme (private joke 2019 de sorry)'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('excusemewtf')
-				.setDescription('Send `Excuse me WTF` meme'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('ehehboi')
-				.setDescription('Send eh eh boi meme')),
+		.addStringOption(option =>
+			option.setName('meme_name')
+				.setDescription('The meme name')
+				.setRequired(true)
+				.addChoice('FBI', 'fbi')
+				.addChoice('Police', 'police')
+				.addChoice('Excuse Me Wtf', 'excusemewtf')
+				.addChoice('Eh Eh Boi', 'ehehboi')),
+
 	async execute(interaction) {
-		if (interaction.options.getSubcommand() === 'ehehboi') {
+		const choice = interaction.options.getString('meme_name');
+		console.log(choice);
+		if (choice === 'ehehboi') {
 			await interaction.reply({ files: [EhehImageList[Math.floor(Math.random() * EhehImageList.length)]] });
-		} else if (interaction.options.getSubcommand() === 'excusemewtf') {
+		} 
+		else if (choice === 'excusemewtf') {
 			await interaction.reply({ files: [ExcusemeImageList[Math.floor(Math.random() * ExcusemeImageList.length)]] });
 		}
-		else if (interaction.options.getSubcommand() === 'fbi') {
+		else if (choice === 'fbi') {
 			await interaction.reply({ files: ['./img/fbi.gif'] });
 		}
-		else if (interaction.options.getSubcommand() === 'police') {
+		else if (choice === 'police') {
 			await interaction.reply({ files: ['./img/police.jpg'] });
 		}
 	},
